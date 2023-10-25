44304f64e74a68a4ea3ed567daee69b9cd41f67c
Anas Ahmed <anasahmed700@gmail.com>
2023-10-25 15:52:14 +0500

    react components & css modules

---
+++ frontend/src/components/Note.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styles from '../styles/note.module.css'
+import { Note as NoteModel } from '../models/note';
+import { Card } from 'react-bootstrap';
+
+interface NoteProps {
+    note: NoteModel;
+}
+
+const Note = ({ note }: NoteProps) => {
+    const {title, text, createdAt, updatedAt} = note;
+    return (
+        <Card className={styles.noteCard}>
+            <Card.Body>
+                <Card.Title>
+                    {title}
+                </Card.Title>
+                <Card.Text className={styles.cardText}>
+                    {text}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default Note
